920839cacccef05c2011114ea49b232a2affe448
Alexandr Kikot <academic77@yandex.ru>
2022-02-21 22:31:41 +0300

    Reworked this module, so this one might be a class to work with in the future.

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,24 +3,61 @@ import cv2
 import mediapipe as mp
 
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-mp_draw = mp.solutions.drawing_utils
+class HandDetector:
+    def __init__(self, mode=False, max_num_hands=2,
+                 min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.mode = mode
+        self.max_num_hands = max_num_hands
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(self.mode, self.max_num_hands,
+                                         min_detection_confidence=self.min_detection_confidence,
+                                         min_tracking_confidence=self.min_tracking_confidence)
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.result = self.hands.process(img_rgb)
+
+        if self.result.multi_hand_landmarks:
+            for hand_lms in self.result.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(img, hand_lms, self.mp_hands.HAND_CONNECTIONS)
+        return img
+
+    def find_position(self, img, hand_id=0, draw=True):
+        lm_list = []
+
+        if self.result.multi_hand_landmarks:
+            my_hand = self.result.multi_hand_landmarks[hand_id]
+            for id, lm in enumerate(my_hand.landmark):
+                height, width, c = img.shape
+                cx, cy = int(lm.x * width), int(lm.y * height)
+                lm_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
+        return lm_list
 
 
 def main():
-    cap = cv2.VideoCapture(0)
     previous_time = 0
-    current_time = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
 
     while True:
         success, img = cap.read()
+        img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
+        if len(lm_list) > 0:
+            print(lm_list[0])
 
         current_time = time.time()
-        fps = 1 / (current_time - previous_time)
+        fps = 1/(current_time - previous_time)
         previous_time = current_time
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_ITALIC, 3, (255, 255, 0), 3)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (255, 255, 0), 3)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
