4593c6ca2c9c892f6c1dbb5e76a5691c9281efac
Alexander Kikot <academic77@yandex.ru>
2022-02-21 20:46:32 +0300

    Added X,Y tracker for hand.

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ while True:
 
     if results.multi_hand_landmarks:
         for hand_lms in results.multi_hand_landmarks:
+            for id, lm in enumerate(hand_lms.landmark):
+                h, w, c, = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
             mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
 
     current_time = time.time()
